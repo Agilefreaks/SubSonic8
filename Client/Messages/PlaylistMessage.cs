9e60c170d3d8fb0c6a919a3b3a6bac7331dce2c8
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2012-11-29 14:38:04 +0200

    Added PlaylistMessage

---
+++ Client/Messages/PlaylistMessage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Client.Common.Models;
+
+namespace Subsonic8.Messages
+{
+    public class PlaylistMessage
+    {
+        public List<ISubsonicModel> Queue { get; set; }
+    }
+}
