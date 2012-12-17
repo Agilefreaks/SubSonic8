c7a85446fba1fb8610822d8a3ec1820b344f1ab3
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2012-12-17 16:25:29 +0200

    updated the description method of the MusicDirectoty class to use the Name property instead of the Type

---
+++ Client.Common.Tests/Models/Subsonic/MusicDirectoryTests.cs
@@ -0,0 +1,30 @@
+﻿using Client.Common.Models.Subsonic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Client.Common.Tests.Models.Subsonic
+{
+    [TestClass]
+    public class MusicDirectoryTests
+    {
+        private MusicDirectory _subject;
+
+        [TestInitialize]
+         public void Setup()
+        {
+            _subject = new MusicDirectory();
+        }
+
+        [TestMethod]
+        public void GetDescription_Always_ReturnsTheCorrectValue()
+        {
+            _subject.Name = "test";
+            _subject.Id = 2;
+
+            var description = _subject.GetDescription();
+
+            description.Item1.Should().Be("test");
+            description.Item2.Should().Be("2");
+        }
+    }
+}
