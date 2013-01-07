e032b194d435193006dc10ba606f1569264770de
cristibadila <Cristi.Badila@gmail.com>
2013-01-07 10:53:52 +0200

    Fixed a bug in PlaybackViewModel which prevented the play all functionality to work due to the fact the bottom bar was not injected when the message was received

--- Client/Framework/ViewModel/ViewModelBase.cs
+++ Client/Framework/ViewModel/ViewModelBase.cs
@@ -86,16 +86,18 @@ namespace Subsonic8.Framework.ViewModel
             SubsonicService = IoC.Get<ISubsonicService>();
             NavigationService = IoC.Get<INavigationService>();
             UpdateDisplayName = () => DisplayName = "Subsonic8";
+            // ReSharper disable DoNotCallOverridableMethodsInConstructor
+            SetBottomBar();
+            // ReSharper restore DoNotCallOverridableMethodsInConstructor
         }
 
         protected override void OnActivate()
         {
             base.OnActivate();
-            SetBottomBar(IoC.Get<IShellViewModel>());
             UpdateDisplayName();
         }
 
-        protected virtual void SetBottomBar(IShellViewModel shell)
+        protected virtual void SetBottomBar()
         {
             BottomBar = IoC.Get<IDefaultBottomBarViewModel>();
         }
