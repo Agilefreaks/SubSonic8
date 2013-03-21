12164148cc790b562b5e2843e866f9aac12adf18
cristibadila <Cristi.Badila@gmail.com>
2013-03-21 11:40:46 +0200

    added the suspension manager and suspension manager exception classes tit the client assembly

---
+++ Client/Framework/SuspensionManagerException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Subsonic8.Framework
+{
+    public class SuspensionManagerException : Exception
+    {
+        public SuspensionManagerException()
+        {
+        }
+
+        public SuspensionManagerException(Exception e)
+            : base("SuspensionManager failed", e)
+        {
+        }
+    }
+}
