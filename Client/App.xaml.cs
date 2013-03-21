d483f09f67aa273fa08ed6263ae7d457fcf0f295
cristibadila <Cristi.Badila@gmail.com>
2013-03-21 17:42:37 +0200

    extracted the loading of application settings into a settings helper to remove code from the app class

--- Client/App.xaml.cs
+++ Client/App.xaml.cs
@@ -1,13 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Caliburn.Micro;
 using Client.Common;
 using Client.Common.Services;
-using Subsonic8.BottomBar;
 using Subsonic8.Framework;
-using Subsonic8.Framework.Services;
+using Subsonic8.Framework.Interfaces;
 using Subsonic8.Main;
 using Subsonic8.Playback;
-using Subsonic8.Settings;
 using Subsonic8.Shell;
 using Subsonic8.VideoPlayback;
 using Windows.ApplicationModel.Activation;
@@ -21,10 +21,7 @@ namespace Subsonic8
     {
         private IShellViewModel _shellViewModel;
         private ApplicationExecutionState _previousExecutionState;
-        private ISubsonicService _subsonicService;
         private CustomFrameAdapter _navigationService;
-        private IToastNotificationService _toastNotificationService;
-        private IStorageService _storageService;
 
         public App()
         {
@@ -72,11 +69,9 @@ namespace Subsonic8
 
             InstantiateRequiredSingletons();
 
-            LoadServices();
-
             BindShellViewModelToView(shellView);
 
-            await LoadSettings();
+            await Kernel.Get<ISettingsHelper>().LoadSettings();
 
             await RestoreLastViewOrGoToMain(shellView);
         }
@@ -92,19 +87,11 @@ namespace Subsonic8
             Kernel.Bind<INavigationService>().ToConstant(_navigationService);
         }
 
-        private void LoadServices()
-        {
-            _subsonicService = Kernel.Get<ISubsonicService>();
-            _toastNotificationService = Kernel.Get<IToastNotificationService>();
-            _storageService = Kernel.Get<IStorageService>();
-        }
-
         private void InstantiateRequiredSingletons()
         {
             //resolved so that they can start listening for events
             Kernel.Get<IPlaybackViewModel>();
             Kernel.Get<IFullScreenVideoPlaybackViewModel>();
-            Kernel.Get<IDefaultBottomBarViewModel>();
         }
 
         private void BindShellViewModelToView(ShellView shellView)
@@ -133,22 +120,6 @@ namespace Subsonic8
             {
                 _navigationService.NavigateToViewModel<MainViewModel>();
             }
-        }
-
-        private async Task LoadSettings()
-        {
-            var subsonic8Configuration = await GetSubsonic8Configuration();
-
-            _subsonicService.Configuration = subsonic8Configuration.SubsonicServiceConfiguration;
-
-            _toastNotificationService.UseSound = subsonic8Configuration.ToastsUseSound;
-        }
-
-        private async Task<Subsonic8Configuration> GetSubsonic8Configuration()
-        {
-            var subsonic8Configuration = await _storageService.Load<Subsonic8Configuration>() ?? new Subsonic8Configuration();
-
-            return subsonic8Configuration;
         }
     }
 }
