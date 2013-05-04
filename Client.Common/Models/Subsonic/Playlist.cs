17a5fbf712ecbba8abc4d252e7b13c540078f205
cristibadila <Cristi.Badila@gmail.com>
2013-05-04 12:54:27 +0300

    added the Playlist and PlaylistEntry classes

---
+++ Client.Common/Models/Subsonic/Playlist.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Client.Common.Models.Subsonic
+{
+    [XmlRoot(ElementName = "Playlist", Namespace = "http://subsonic.org/restapi")]
+    public class Playlist : SubsonicModelBase
+    {
+        [XmlAttribute("fan")]
+        public string Fan { get; set; }
+
+        [XmlAttribute("owner")]
+        public string Owner { get; set; }
+
+        [XmlAttribute("public")]
+        public bool Public { get; set; }
+
+        [XmlAttribute("songCount")]
+        public int SongCount { get; set; }
+
+        [XmlAttribute("duration")]
+        public int Duration { get; set; }
+
+        [XmlAttribute("created")]
+        public DateTime Created { get; set; }
+
+        [XmlElement(ElementName = "entry", Namespace = "http://subsonic.org/restapi")]
+        public List<MusicDirectoryChild> Entries { get; set; }
+
+        public override SubsonicModelTypeEnum Type
+        {
+            get { return SubsonicModelTypeEnum.Playlist; }
+        }
+    }
+}
---
+++ Client.Common/Models/Subsonic/PlaylistEntry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Client.Common.Models.Subsonic
+{
+    public class PlaylistEntry : MediaModelBase
+    {
+        [XmlAttribute("title")]
+        public override string Name { get; set; }
+
+        [XmlAttribute("paArent")]
+        public int ParentId { get; set; }
+
+        [XmlAttribute("album")]
+        public string Album { get; set; }
+
+        [XmlAttribute("artist")]
+        public string Artist { get; set; }
+
+        [XmlAttribute("duration")]
+        public int Duration { get; set; }
+
+        [XmlAttribute("bitRate")]
+        public int BitRate { get; set; }
+
+        [XmlAttribute("track")]
+        public int Track { get; set; }
+
+        [XmlAttribute("year")]
+        public int Year { get; set; }
+
+        [XmlAttribute("genre")]
+        public int Genre { get; set; }
+
+        [XmlAttribute("isVideo")]
+        public int IsVideo { get; set; }
+
+        [XmlAttribute("albumId")]
+        public int AlbumId { get; set; }
+
+        [XmlAttribute("artistId")]
+        public int ArtistId { get; set; }
+
+        public override SubsonicModelTypeEnum Type
+        {
+            get { return SubsonicModelTypeEnum.Folder; }
+        }
+
+        public override Tuple<string, string> GetDescription()
+        {
+            return new Tuple<string, string>(Name, string.Format("Artist: {0}, Album: {1}", Artist, Album));
+        }
+    }
+}
