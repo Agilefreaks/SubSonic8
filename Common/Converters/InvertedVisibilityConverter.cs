1600b6a9178584fefe2354bfec41b2bfaf132cd2
cristibadila <Cristi.Badila@gmail.com>
2013-11-08 15:26:24 +0200

    fixes weird issue with the 'Nothing playing' and artist details button being visible at the same time even though they were bound to the same property and used opposite converters

--- Common/Converters/InvertedVisibilityConverter.cs
+++ Common/Converters/InvertedVisibilityConverter.cs
@@ -4,11 +4,13 @@
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Data;
 
-    public class InvertedNullToVisibilityConverter : IValueConverter
+    public class InvertedVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            return value == null ? Visibility.Visible : Visibility.Collapsed;
+            return (value is Visibility && (Visibility)value == Visibility.Collapsed)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
