66374c9855ffa8cf33ac29c698e6172ac527af66
pirvudoru <pirvu.doru@ymail.com>
2015-08-11 09:14:46 +0300

    [#123] prepares converter

---
+++ Client.Tests/Converters/StringToVisibilityConverterTests.cs
@@ -0,0 +1,53 @@
+﻿namespace Client.Tests.Converters
+{
+    using System;
+    using Windows.UI.Xaml;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+    using Subsonic8.Converters;
+
+    [TestClass]
+    public class StringToVisibilityConverterTests
+    {
+        #region Fields
+
+        private StringToVisibilityConverter _subject;
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _subject = new StringToVisibilityConverter();
+        }
+
+        [TestMethod]
+        public void ConvertWhenArgumentIsNullReturnsCollapsed()
+        {
+            var result = (Visibility)_subject.Convert(null, null, null, null);
+
+            result.Should().Be(Visibility.Collapsed);
+        }
+
+        [TestMethod]
+        public void ConvertWhenArgumentIsEmptyStringReturnsCollapsed()
+        {
+            var result = (Visibility)_subject.Convert(string.Empty, null, null, null);
+
+            result.Should().Be(Visibility.Collapsed);
+        }
+
+        [TestMethod]
+        public void ConvertWhenArgumentIsNonEmptyStringReturnsVisible()
+        {
+            // Tip: You will have to write this test to verify that when 
+            // you pass a non empty string as value argument
+            // you will receive as output the Visibility.Visible value (See examples above)
+            throw new NotImplementedException();
+        }
+        
+        #endregion
+    }
+}
---
+++ Client/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Subsonic8.Converters
+{
+    using System;
+    using Windows.UI.Xaml.Data;
+
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public Object Convert(Object value, System.Type targetType, Object parameter, string language)
+        {
+            // Tip: The value to check is "value" argument. It is a generic object.
+            // You will have to cast it to string then verify if it is null or empty.
+            // Use "string.IsNullOrEmpty" method
+
+            throw new System.NotImplementedException();
+        }
+
+        public Object ConvertBack(Object value, System.Type targetType, Object parameter, string language)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
