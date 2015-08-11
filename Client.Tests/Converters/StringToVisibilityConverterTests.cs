8698fe3a00e0a835539e5803c2ce87eab7b94ccd
cristi-badila <Cristi.Badila@gmail.com>
2015-08-11 08:15:35 +0300

    [#123] adds some groundwork for browse view improvements

---
+++ Client.Tests/Converters/StringToVisibilityConverterTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Client.Tests.Converters
+{
+    using Windows.UI.Xaml;
+
+    using FluentAssertions;
+
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+    using Subsonic8.Converters;
+
+    [TestClass]
+    public class StringToVisibilityConverterTests
+    {
+        private StringToVisibilityConverter _subject;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _subject = new StringToVisibilityConverter();
+        }
+
+        [TestMethod]
+        public void Convert_ValueIsAnEmptyString_ReturnsVisibilityCollapsed()
+        {
+            var result = _subject.Convert(string.Empty, null, null, null);
+
+            result.Should().Be(Visibility.Collapsed);
+        }
+    }
+}
---
+++ Client/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace Subsonic8.Converters
+{
+    using System;
+
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Data;
+
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
