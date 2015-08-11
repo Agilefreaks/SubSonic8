e5847354e42e7476fb44dba28205dd044302dd09
AnaPreda <preda_ana20@yahoo.com>
2015-08-11 12:16:06 +0300

    [#123] modifies the Artist model so as to have an empty string as its subtitle

--- Client.Common.Tests/Models/Subsonic/ArtistTests.cs
+++ Client.Common.Tests/Models/Subsonic/ArtistTests.cs
@@ -23,7 +23,7 @@
             const string Name = "test_n";
             _subject.Name = Name;
 
-            _subject.GetDescription().Should().Be(new Tuple<string, string>(Name, Name));
+            _subject.GetDescription().Should().Be(new Tuple<string, string>(Name, ""));
         }
 
         [TestMethod]
